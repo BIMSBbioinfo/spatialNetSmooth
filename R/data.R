8708cb7daa98aeec5ebb2d20484ce51c9bb0afc5
raheb61 <rbergho@cl-akalin13.mdc-berlin.net>
2024-04-22 17:09:20 +0200

    edited example-data

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' List of human genes
+#'
+#' Genes where high expression is 
+#' either associated with normal cells or cancer cells
+#'obtained using Ikarus
+#'https://github.com/BIMSBbioinfo/ikarus
+#'
+#' @format A list of genes with with Type "Tumor" or "Normal"
+#' @source ?
+#' @example 
+#' data(geneset_ikarus)
+"geneset_ikarus"
+
+#' Human breast-cancer-tissue
+#'
+#' An Seurat-Object containing spatial and gene-expression data of human breats-tissue with cancer
+#' 
+#'
+#' @format A Seurat-Object
+#' @source ?
+#' @example
+#' data(seu)
+"seu"
+
